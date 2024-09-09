d79cfc337b34c2d21ba0a3ede059378866e35ae4
natalybarah <natalykodes@gmail.com>
2024-09-09 07:36:34 -0700

    adding env variables

--- Smart Brain API/server.js
+++ Smart Brain API/server.js
@@ -13,14 +13,18 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-      host: 'process.env.DATABASE_HOST',
+      host: process.env.DATABASE_HOST,
       port: 5432,
-      user: 'process.env.DATABASE_USER',
-      password: 'process.env.DATABASE_PW',
-      database: 'process.env.DATABASE_DB', //NOMBRE DE LA DATABASE? SMART_BRAIN
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PW,
+      database: process.env.DATABASE_DB, //NOMBRE DE LA DATABASE? SMART_BRAIN
     },
   });
-
+  console.log('Database Host:', process.env.DATABASE_HOST);
+console.log('Database User:', process.env.DATABASE_USER);
+console.log('Database Password:', process.env.DATABASE_PW);
+console.log('Database Name:', process.env.DATABASE_DB);
+console.log(process.env);
   db.select('*').from('users').then(data=>{
     console.log(data)
   })
